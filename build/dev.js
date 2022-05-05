7dfdf4fa4766c4e1267129c2d09e3e9e515d00e9
Lin.xs <yunfax@outlook.com>
2022-05-05 23:33:24 +0800

    fix: 修复匹配 url 问题

--- build/dev.js
+++ build/dev.js
@@ -5,7 +5,6 @@ import { sassCmd, rollupCmd } from "./command.js";
 const inputCss = path.resolve("./src/styles/dark-support.css");
 const outputCss = path.resolve("./dist/styles/dark-support.css");
 
-console.log(sassCmd.opts(inputCss, outputCss, true));
 const sassFork = fork(sassCmd.cmd, sassCmd.opts(inputCss, outputCss, true));
 sassFork.on("error", (err) => {
   console.log("[Sass Fork Error]", err);
